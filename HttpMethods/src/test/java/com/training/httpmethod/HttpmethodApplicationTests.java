0ea37d005466ae10a72bd400288608a19a2a11fd
RincisM <rincismelvin@gmail.com>
2024-06-27 10:25:33 +0530

    Created a New SpringBoot Project HttpMethods

---
+++ HttpMethods/src/test/java/com/training/httpmethod/HttpmethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.httpmethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpmethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
